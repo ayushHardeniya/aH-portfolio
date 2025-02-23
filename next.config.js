a17611579ea9c42725339a8243c334d2be21e598
Ayush Sharma <ayushhardeniya.grid@gmail.com>
2025-02-24 02:09:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // You can add more configuration options here if needed
+};
+
+module.exports = nextConfig;
