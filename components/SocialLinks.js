ba207cdd4250cc1eba1f2ab81ceadff23f59390e
Ayush Sharma <ayushhardeniya.grid@gmail.com>
2025-02-24 03:06:26 +0530

    Create SocialLinks.js

---
+++ components/SocialLinks.js
@@ -0,0 +1,15 @@
+const SocialLinks = () => {
+  return (
+    <div className="social-links">
+      <p>Find Ayush Hardeniya on:</p>
+      <a href="https://youtube.com/@ayushhardeniya" target="_blank">YouTube</a>
+      <a href="https://instagram.com/ayushhardeniya" target="_blank">Instagram</a>
+      <a href="https://twitter.com/ayushhardeniya" target="_blank">Twitter</a>
+      <a href="https://linkedin.com/in/ayushhardeniya" target="_blank">LinkedIn</a>
+      <a href="https://facebook.com/ayushhardeniya" target="_blank">Facebook</a>
+      <a href="https://github.com/ayushhardeniya" target="_blank">GitHub</a>
+    </div>
+  );
+};
+
+export default SocialLinks;
