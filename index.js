75456d9235222ded679b1f0d9ef9bae760bc132a
Ayush Sharma <ayushhardeniya.grid@gmail.com>
2025-03-01 14:34:55 +0530

    Update and rename next.js to index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ export default function Home() {
       <section
         id="hero"
         className="pt-24 relative h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('/assets/banneraHnew.png')" }}
+        style={{ backgroundImage: "url('/assets/heroaH.jpg')" }}
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
